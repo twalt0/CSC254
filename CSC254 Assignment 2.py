66a53941209bfc982fab731ffd4e6dd4bb382ba1
twalt0 <140025205+twalt0@users.noreply.github.com>
2023-09-22 19:16:58 -0400

    Update CSC254 Assignment 2.py

--- CSC254 Assignment 2.py
+++ CSC254 Assignment 2.py
@@ -2,30 +2,52 @@
 
 import psycopg2
 
-command1 = ("""
-        CREATE TABLE school_info (
+table_commands = ("""
+        CREATE TABLE students (
             student_id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            gpa_100 INTEGER
+            fafsa BOOLEAN
+        )
+        ""","""
+        CREATE TABLE grades (
+            student_id SERIAL PRIMARY KEY,
+            gpa_100 INTEGER,
+            csc254 INTEGER
         )
         """)
-insert_query = """
-INSERT INTO school_info (student_id, name, gpa_100)
-VALUES (%s, %s, %s)
-"""
 
-data_to_insert = [(70000001, 'Thomas Walter', 100),
-                  (70000002, 'Vincent Gogh', 75),
-                  (70000003, 'Salvador Dali', 80),
-                  (70000004, 'Francisco Goya', 60)]
+insert_query = ["""
+INSERT INTO students (student_id, name, fafsa)
+VALUES (%s, %s, %s)
+""","""
+INSERT INTO grades (student_id, gpa_100, csc254)
+VALUES (%s, %s, %s)
+"""]
+
+students_to_insert = [(70000001, 'Thomas Walter', False),
+                  (70000002, 'Vincent Gogh', True),
+                  (70000003, 'Salvador Dali', False),
+                  (70000004, 'Francisco Goya', True)]
+
+grades_to_insert = [(70000001, 100, 100),
+                  (70000002, 75, 85),
+                  (70000003, 80, 80),
+                  (70000004, 60, 50)]
 
 conn = None
 try:
-    conn = psycopg2.connect(host="localhost", port=5432, dbname="postgres", user="postgres", password="fakepasswordlolgottem")
+    conn = psycopg2.connect(host="localhost", port=5432, dbname="postgres", user="postgres", password="Womster*0808*")
     cur = conn.cursor()
-    cur.execute(command1)
-    for values in data_to_insert:
-        cur.execute(insert_query, values)
+        
+    for values in table_commands:
+        cur.execute(values)
+            
+    for values in students_to_insert:
+        cur.execute(insert_query[0], values)
+        
+    for values in grades_to_insert:
+        cur.execute(insert_query[1], values)
+        
     cur.close()
     conn.commit()
 except (Exception, psycopg2.DatabaseError) as error:
